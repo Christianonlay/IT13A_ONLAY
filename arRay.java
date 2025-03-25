5009004eae2cc3dfcff98fc2e6e3621f9ee22175
Christianonlay <onlaychristian99@gmail.com>
2025-03-25 08:39:59 +0800

    Add files via upload

---
+++ arRay.java
@@ -0,0 +1,40 @@
+package onlay;
+//
+///**
+// *
+// * @author User
+// */
+//public class Array.java {
+//  public static void main (String[] args){
+//      String[] array = {"A", "B", "C", "D", "F", "G"};
+//      for (String letter : array){
+//          System.out.print(letter + ",");
+//      }
+//      
+//  }  
+//}
+public class arRay {
+    
+    public static void main(String[] args) {
+       int[][] array = {
+           {1, 2, 3, 4},
+           {5, 6, 7, 8},
+           {9, 10, 11, 12}
+           
+       };
+      
+    
+           for (int i = 0; i < array.length; i++){
+               
+    System.out.print("The elements in row" + (i + 1) + ":");
+    for(int j = 0; j < array[i].length; j++)
+    {
+        System.out.print (array[i][j] + " ");
+    }
+        
+     System.out.println();
+    }
+}
+               
+    }         
+             
